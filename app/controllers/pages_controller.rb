d625c5906d668e391608b47435fd1f51206bc270
Randall Hansen <randall@opensourcery.com>
2011-02-18 12:51:27 -0800

    * creating new branch from work in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,35 @@ class PagesController < ApplicationController
     @recently_reported_nodes = Node.reported.by_report_date.unhidden.all(:limit => 10)
 
     @unhidden_nodes = Node.unhidden
+
+    # XXX this is temporary mockup data
+    # aww yeah, brute force FTW
+    @node_summaries = [
+      {
+        :label => 'unresponsive',
+        :count => @no_longer_reporting_nodes.count + @unreported_nodes.count,
+      },
+      {
+        :label => 'failed',
+        :count => @currently_failing_nodes.count,
+      },
+#       {
+#         :label => 'out-of-audit', # need to define exactly what this is
+#         :count => 0,
+#       },
+      {
+        :label => 'pending', # need real data access API
+        :count => 22,
+      },
+      {
+        :label => 'compliant', # needs to wait on the above two
+        :count => 109,
+      },
+    ]
+    @total_nodes = @unhidden_nodes.count
+    @node_summaries.each do |n| # round to one decimal
+      n[ :percent ] = (( n[ :count ].to_f / @total_nodes.to_f ) * 1000 ).round.to_f / 10
+    end
   end
 
   def release_notes
